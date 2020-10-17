d109e43f015d279fe72970132876c77388a8b73a
Vlad492 <skrinka2604@gmail.com>
2020-10-17 16:48:24 +0300

    change

--- router/api/messages/index.js
+++ router/api/messages/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const bodyParser = require('body-parser')
 router.use(bodyParser.urlencoded({ extended: false }))
-const Messages = require('../../../constrollers/messagesControllers/controllers')
+const Messages = require('../../../controllers/messagesControllers/controllers')
 
 router.get('/api/messages/list/:number', Messages.messageListNumber)//get messages
 router.get('/api/messages/single/:id', Messages.messageSingleId)//get message by id
--- router/api/users/index.js
+++ router/api/users/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const bodyParser = require('body-parser')
 router.use(bodyParser.urlencoded({ extended: false }))
-const Users = require('../../../constrollers/usersControllers/controllers')
+const Users = require('../../../controllers/usersControllers/controllers')
 
 router.post('/api/users/create', Users.create)//create a new user
 
